1b49133bda05175c81d8b3723b69ccf07661c712
qulbyazz <qulbyazzumy@gmail.com>
2024-10-09 13:23:49 +0700

    update percobaan 1

--- SiakadFor23.java
+++ SiakadFor23.java
@@ -6,6 +6,7 @@ public class SiakadFor23 {
         Scanner input = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0; 
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -17,9 +18,17 @@ public class SiakadFor23 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            }else{
+                tidakLulus++;
+            }
         }
 
         System.out.println("Nilai Tertinggi: " + tertinggi);
         System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Jumlah Mhs Lulus: " +lulus);
+        System.out.println("Jumlah Mhs Tidak Lulus: " +tidakLulus);
     }
 }
