204f5e7b0694c54df01aedf73422f9b8d924ab2c
qulbyazz <qulbyazzumy@gmail.com>
2024-10-09 12:39:48 +0700

    percobaan 1

---
+++ SiakadFor23.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SiakadFor23 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double nilai, tertinggi = 0, terendah = 100;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = input.nextDouble();
+
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+    }
+}
